fd01a373029e24d5cca2b630c8312fd555085a8a
Bess Lin <bess861231@gmail.com>
2017-12-11 21:46:12 +0800

    Func1:Roll the dice

--- app/src/main/java/com/fju/mid2/Func1Activity.java
+++ app/src/main/java/com/fju/mid2/Func1Activity.java
@@ -1,8 +1,12 @@
 package com.fju.mid2;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Random;
 
 public class Func1Activity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class Func1Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_func1);
+    }
+
+    public void roll(View view){
+        int n = new Random().nextInt(6)+1;
+        String num = Integer.toString(n);
+        new AlertDialog.Builder(this)
+                .setMessage(num)
+                .setPositiveButton("OK", null)
+                .show();
     }
 }
